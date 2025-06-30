b291d06db6652749bf32b3c47b77d6a4c5d61fe1
Tim Douglas <neurotech@gmail.com>
2025-06-30 19:17:20 +1000

    Stubbed out a basic app component structure

---
+++ app/balance/add/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/header";
+import { Separator } from "../../components/separator";
+
+export default function AddBalance() {
+  return (
+    <>
+      <Header label={"Add Balance"} />
+      <Separator />
+      <section>Placeholder</section>
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/app/components/header";
+import { Separator } from "./components/separator";
+
+export default function Home() {
+  return (
+    <>
+      <Header label={"Home"} />
+      <Separator />
+      <section>Placeholder</section>
+    </>
+  );
+}
