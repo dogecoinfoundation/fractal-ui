2708bff85ef7afc92060d2ee75747fcf3e23dfd8
Tim Douglas <neurotech@gmail.com>
2025-07-09 21:55:03 +1000

    Extracted out mints sidebar into it's own component

--- app/mint/list/page.tsx
+++ app/mint/list/page.tsx
@@ -3,9 +3,9 @@
 import { useState } from "react";
 import useSWR from "swr";
 import type { Mint } from "@/app/api/mints/route";
+import { MintsSidebar } from "@/components/mints/list/mints-sidebar";
 import { MintCard } from "@/components/mints/mint-card";
 import { Input } from "@/components/ui/input";
-import { Skeleton } from "@/components/ui/skeleton";
 import { Paper } from "@/components/ui/surfaces/Paper";
 import { cn } from "@/lib/utils";
 
@@ -32,41 +32,13 @@ export default function ListMints() {
             />
           </div>
 
-          <div className="flex flex-col h-full overflow-auto border-t-1 border-gray-300 bg-white">
-            {isLoading ? (
-              <div className="flex flex-col justify-start items-start h-full p-2 gap-2">
-                {Array.from({ length: 10 }).map((_, index) => (
-                  // biome-ignore lint/suspicious/noArrayIndexKey: <Not that crucial for multiple skeleton placeholders.>
-                  <Skeleton key={index} className="h-12 w-full rounded-xs" />
-                ))}
-              </div>
-            ) : (
-              data
-                ?.filter((mint) =>
-                  mint.title.toLowerCase().includes(filterText.toLowerCase()),
-                )
-                .map((mint) => (
-                  <button
-                    type="button"
-                    key={mint.id}
-                    className={cn(
-                      "flex flex-col gap-1 w-full justify-start items-center cursor-pointer text-left text-sm bg-white/80 hover:bg-gray-100 transition-colors p-2",
-                      activeMint?.id === mint.id
-                        ? "bg-blue-100/65 hover:bg-blue-100/65 [&>h2]:text-blue-900 [&>h3]:text-blue-900/85"
-                        : "",
-                    )}
-                    onClick={() => setActiveMint(mint)}
-                  >
-                    <h2 className="font-semibold text-gray-900 leading-tight text-left w-full">
-                      {mint.title}
-                    </h2>
-                    <h3 className="text-xs text-gray-600 text-left w-full">
-                      {mint.description}
-                    </h3>
-                  </button>
-                ))
-            )}
-          </div>
+          <MintsSidebar
+            isLoading={isLoading}
+            filterText={filterText}
+            data={data}
+            setActiveMint={setActiveMint}
+            activeMint={activeMint}
+          />
         </div>
 
         <div className="flex-col flex-1 p-2">
---
+++ components/mints/list/mints-sidebar.tsx
@@ -0,0 +1,57 @@
+import type { Mint } from "@/app/api/mints/route";
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
+
+interface MintsSidebarProps {
+  isLoading: boolean;
+  filterText: string;
+  activeMint: Mint | null;
+  setActiveMint: (mint: Mint) => void;
+  data?: Mint[];
+}
+
+export const MintsSidebar = ({
+  isLoading,
+  filterText,
+  data,
+  setActiveMint,
+  activeMint,
+}: MintsSidebarProps) => {
+  return (
+    <div className="flex flex-col h-full overflow-auto border-t-1 border-gray-300 bg-white select-none">
+      {isLoading ? (
+        <div className="flex flex-col justify-start items-start h-full p-2 gap-2">
+          {Array.from({ length: 10 }).map((_, index) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: <Not that crucial for multiple skeleton placeholders.>
+            <Skeleton key={index} className="h-12 w-full rounded-xs" />
+          ))}
+        </div>
+      ) : (
+        data
+          ?.filter((mint) =>
+            mint.title.toLowerCase().includes(filterText.toLowerCase()),
+          )
+          .map((mint) => (
+            <button
+              type="button"
+              key={mint.id}
+              className={cn(
+                "flex flex-col gap-1 w-full justify-start items-center cursor-pointer text-left text-sm bg-white/80 hover:bg-gray-100 transition-colors p-2",
+                activeMint?.id === mint.id
+                  ? "bg-blue-100/65 hover:bg-blue-100/65 [&>h2]:text-blue-900 [&>h3]:text-blue-900/85"
+                  : "",
+              )}
+              onClick={() => setActiveMint(mint)}
+            >
+              <h2 className="font-semibold text-gray-900 leading-tight text-left w-full">
+                {mint.title}
+              </h2>
+              <h3 className="text-xs text-gray-600 text-left w-full">
+                {mint.description}
+              </h3>
+            </button>
+          ))
+      )}
+    </div>
+  );
+};
