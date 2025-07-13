51ad18dbc257c36d8a937ec1dd99ceddd7dfee75
Tim Douglas <neurotech@gmail.com>
2025-07-14 09:06:46 +1000

    Show wizard if config table is empty

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { getDatabase } from "@/app/database";
 import { Header } from "@/components/header";
 import { Separator } from "@/components/separator";
 import { SideBar } from "@/components/sidebar";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import "./globals.css";
+import { SetupWizard } from "@/components/setup/setup-wizard";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Fractal Admin",
@@ -14,18 +26,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const db = getDatabase();
+  const configData = db.prepare("SELECT * FROM config").all();
+
   return (
     <html lang="en" className="h-full">
       <body className="antialiased h-full">
         <div className="flex flex-row min-h-full" role="document">
-          <SideBar />
-          <main className="flex flex-col flex-1">
-            <div className="flex flex-col h-screen gap-3 p-4">
-              <Header />
-              <Separator />
-              {children}
-            </div>
-          </main>
+          {configData.length <= 0 ? (
+            <SetupWizard />
+          ) : (
+            <>
+              <SideBar />
+              <main className="flex flex-col flex-1">
+                <div className="flex flex-col h-screen gap-3 p-4">
+                  <Header />
+                  <Separator />
+                  {children}
+                </div>
+              </main>
+            </>
+          )}
         </div>
       </body>
     </html>
