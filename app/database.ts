44e549101e8b50369c99c9eb01bb3e999c5de32e
Tim Douglas <neurotech@gmail.com>
2025-07-15 15:30:39 +1000

    Removed better-sqlite3

--- app/database.ts
+++ app/database.ts
@@ -1,20 +1,2 @@
-import Database from "better-sqlite3";
 
-export const getDatabase = () => {
-  const dbPath = "database.sqlite";
-  const db = new Database(dbPath);
-
-  return db;
 };
-
-export const getAllRows = <T>(table: string): T[] =>
-  getDatabase().prepare(`SELECT * FROM ${table}`).all() as T[];
-
-export const getRowByColumnValue = <T>(
-  table: string,
-  columnName: string,
-  value: string,
-): T | null =>
-  getDatabase()
-    .prepare(`SELECT * FROM ${table} WHERE ${columnName} = ?`)
-    .get(value) as T | null;
