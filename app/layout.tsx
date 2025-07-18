3c156b416acef7f46530928fab1224cce3b624a2
Daniel Whelan <chobomuffin@gmail.com>
2025-07-18 17:18:20 +0100

    fu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,39 @@
+"use client"
+
+import { CONFIG_KEYS } from "@/lib/definitions";
 import "./globals.css";
+import { SetupWizard } from "@/components/setup/setup-wizard";
+import { SideBar } from "@/components/sidebar";
+import { Header } from "@/components/header";
+import { Separator } from "@/components/separator";
+import { useAPI } from "@/hooks/useAPI";
+import { Config } from "@/generated/prisma";
 
-import DynamicLayoutWrapper from "@/components/root/dynamic-wrapper";
-
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
- 
+  const { data, isLoading, error } = useAPI<Config[]>("/api/config");
+
   return (
     <html lang="en" className="h-full">
       <body className="antialiased h-full">
         <div className="flex flex-row min-h-full" role="document">
-          <DynamicLayoutWrapper>
-            {children}
-          </DynamicLayoutWrapper>
+          {data && data.length < CONFIG_KEYS.length ? (
+            <SetupWizard />
+          ) : (
+            <>
+              <SideBar />
+              <main className="flex flex-col flex-1">
+                <div className="flex flex-col h-screen gap-3 p-4">
+                  <Header />
+                  <Separator />
+                  {children}
+                </div>
+              </main>
+            </>
+          )}
         </div>
       </body>
     </html>
