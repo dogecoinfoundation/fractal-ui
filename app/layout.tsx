5afc6a42b42ef61f893511402b9819c5f2efd52e
Daniel Whelan <chobomuffin@gmail.com>
2025-07-18 16:51:55 +0100

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,13 @@
 import "./globals.css";
 
-import { Header } from "@/components/header";
 import DynamicLayoutWrapper from "@/components/root/dynamic-wrapper";
-import { Separator } from "@/components/separator";
-import { SetupWizard } from "@/components/setup/setup-wizard";
-import { SideBar } from "@/components/sidebar";
-import { PrismaClient } from "@/generated/prisma";
-import { CONFIG_KEYS } from "@/lib/definitions";
-
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const prisma = new PrismaClient();
-  const configData = await prisma.config.findMany();
-
+ 
   return (
     <html lang="en" className="h-full">
       <body className="antialiased h-full">
