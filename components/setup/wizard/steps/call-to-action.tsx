9e161c342cb8618d73ce6b90731adbd08d1f8c56
Tim Douglas <neurotech@gmail.com>
2025-07-28 11:48:18 +1000

    Tweaked loading styles in CallToAction

--- components/setup/wizard/steps/call-to-action.tsx
+++ components/setup/wizard/steps/call-to-action.tsx
@@ -35,7 +35,7 @@ const messageVariants = cva(
         error: "border-red-500/50 bg-red-50 text-red-700",
         saved: "border-emerald-400 bg-emerald-50 text-emerald-700",
         unsaved: "border-amber-400 bg-amber-50 text-amber-700",
-        loading: "border-orange-500/50 bg-orange-50 text-orange-700",
+        loading: "border-zinc-300 bg-zinc-50 text-zinc-400",
         dirty: "border-amber-400 bg-amber-50 text-amber-700",
         empty: "border-blue-500/50 bg-blue-50 text-blue-600",
         idle: "border-zinc-300 bg-zinc-50 text-zinc-400",
@@ -55,7 +55,8 @@ const buttonVariants = cva(
         error: "bg-red-500 hover:bg-red-700",
         saved: "",
         unsaved: "",
-        loading: "bg-orange-500",
+        loading:
+          "bg-zinc-300 hover:bg-zinc-300 disabled:bg-zinc-300 disabled:opacity-100",
         dirty: "bg-amber-500 hover:bg-amber-400",
         empty: "bg-blue-500 hover:bg-blue-400",
         idle: "bg-zinc-300 hover:bg-zinc-300 disabled:bg-zinc-300 disabled:opacity-100",
@@ -81,7 +82,7 @@ export const CallToAction = ({
         status: "loading",
         message: "Loading...",
         buttonIcon: <Loader2 className="animate-spin" />,
-        buttonText: "Saving",
+        buttonText: "Save",
       };
 
     if (error)
