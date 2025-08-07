16e9dece9fd0312a37be23c81de7db417bb64420
Tim Douglas <neurotech@gmail.com>
2025-08-07 14:18:07 +1000

    Added UI for viewing and confirming a seed phrase

---
+++ components/wallet/create/seed-phrase/seed-phrase-tile.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import { cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+type SeedPhraseTileProps = {
+  word: string;
+  position: number;
+  isCandidate?: boolean;
+  valid?: boolean;
+  invalid?: boolean;
+  dirty?: boolean;
+};
+
+const positionVariants = cva("rounded-l-sm px-2 py-1 select-none", {
+  variants: {
+    isCandidate: {
+      true: null,
+      false: null,
+    },
+    dirty: {
+      true: null,
+      false: "bg-amber-400 text-white border-1 border-amber-400",
+    },
+    valid: {
+      true: null,
+      false: null,
+    },
+    invalid: {
+      true: null,
+      false: null,
+    },
+  },
+  compoundVariants: [
+    // Non-Candidate
+    {
+      isCandidate: false,
+      dirty: false,
+      valid: false,
+      invalid: false,
+      className: "bg-zinc-100 text-zinc-400 border-1 border-zinc-200",
+    },
+    // Candidate and Valid
+    {
+      isCandidate: true,
+      dirty: true,
+      valid: true,
+      invalid: false,
+      className: "bg-green-400 text-white border-1 border-green-400",
+    },
+    // Candidate and Invalid
+    {
+      isCandidate: true,
+      dirty: true,
+      valid: false,
+      invalid: true,
+      className: "bg-red-400 text-white border-1 border-red-400",
+    },
+  ],
+});
+
+const wordVariants = cva(
+  "rounded-r-sm flex-1 px-2 py-1 truncate tabular-nums",
+  {
+    variants: {
+      isCandidate: {
+        true: null,
+        false: null,
+      },
+      dirty: {
+        true: null,
+        false:
+          "bg-amber-50 text-amber-600 border-1 border-amber-400/50 border-l-0",
+      },
+      valid: {
+        true: null,
+        false: null,
+      },
+      invalid: {
+        true: null,
+        false: null,
+      },
+    },
+    compoundVariants: [
+      // Non-Candidate
+      {
+        isCandidate: false,
+        dirty: false,
+        valid: false,
+        invalid: false,
+        className:
+          "bg-zinc-50 text-zinc-600 border-1 border-zinc-200 border-l-0",
+      },
+      // Candidate and Valid
+      {
+        isCandidate: true,
+        dirty: true,
+        valid: true,
+        invalid: false,
+        className:
+          "bg-green-50 text-green-600 border-1 border-green-600/30 border-l-0",
+      },
+      // Candidate and Invalid
+      {
+        isCandidate: true,
+        dirty: true,
+        valid: false,
+        invalid: true,
+        className:
+          "bg-red-50 text-red-600 border-1 border-red-400/50 border-l-0",
+      },
+    ],
+  },
+);
+
+export const SeedPhraseTile = ({
+  word,
+  position,
+  isCandidate,
+  valid,
+  invalid,
+  dirty,
+}: SeedPhraseTileProps) => {
+  return (
+    <div
+      className="flex flex-row items-center font-mono border-0 rounded-sm animate-swing-in text-sm transition-colors shadow-crisp shadow-zinc-200/40"
+      style={{
+        animationDelay: `${position * 40}ms`,
+      }}
+    >
+      <div
+        className={cn(
+          positionVariants({
+            isCandidate,
+            dirty,
+            valid,
+            invalid,
+          }),
+        )}
+      >
+        {position.toString().padStart(2, "0")}
+      </div>
+      <div
+        className={cn(
+          wordVariants({
+            isCandidate,
+            dirty,
+            valid,
+            invalid,
+          }),
+        )}
+      >
+        {word}
+      </div>
+    </div>
+  );
+};
---
+++ components/wallet/create/seed-phrase/seed-phrase-words.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { GridPaper } from "@/components/ui/surfaces/GridPaper";
+import { SeedPhraseTile } from "@/components/wallet/create/seed-phrase/seed-phrase-tile";
+import { cn } from "@/lib/utils";
+
+type SeedPhraseWordsProps = {
+  seedPhrase: string[];
+  confirming?: boolean;
+  mask?: boolean;
+  candidates?: { position: number; word: string }[];
+  allFields?: Record<string, string>;
+  className?: string;
+};
+
+const maskWord = (
+  word: string,
+  candidate?: { input?: string; position: number; word: string },
+  mask?: boolean,
+) => {
+  const maskShape = "************";
+
+  if (candidate) {
+    if (candidate.input === "") return maskShape;
+
+    if (candidate.input === candidate.word) return candidate.input;
+
+    const maskedCharacters = Array.from(
+      { length: maskShape.length - (candidate.input?.length || 0) },
+      () => "*",
+    ).join("");
+
+    return `${candidate.input}${maskedCharacters}`;
+  }
+
+  if (mask) return maskShape;
+
+  return word;
+};
+
+export const SeedPhraseWords = ({
+  seedPhrase,
+  mask,
+  candidates,
+  allFields,
+  className,
+}: SeedPhraseWordsProps) => {
+  const combined = candidates?.map((c, index) => ({
+    ...c,
+    input: allFields?.[`word${index + 1}`]?.toLowerCase(),
+  }));
+
+  return (
+    <GridPaper className={cn(className)}>
+      <div className="grid col-span-3 grid-cols-4 grid-flow-col gap-2 grid-rows-6 bg-blue-50/50 z-10">
+        {seedPhrase.map((word, index) => {
+          const position = index + 1;
+          const candidate = combined?.find((c) => c.position === position);
+
+          let valid = false;
+          let invalid = false;
+          let dirty = false;
+
+          if (candidate) {
+            valid = candidate.word === candidate.input;
+            invalid = candidate.word !== candidate.input;
+            dirty = candidate.input !== "";
+          }
+
+          return (
+            <SeedPhraseTile
+              key={`${position}-${word}`}
+              word={maskWord(word, candidate, mask)}
+              position={position}
+              isCandidate={Boolean(candidate)}
+              valid={valid}
+              invalid={invalid}
+              dirty={dirty}
+            />
+          );
+        })}
+      </div>
+    </GridPaper>
+  );
+};
