acbfb2ea74f09b68b85266347cb348be840371b0
Tim Douglas <neurotech@gmail.com>
2025-08-08 11:04:32 +1000

    Throw 404 when GETting /api/balance if wallet does not exist

--- app/api/balance/route.ts
+++ app/api/balance/route.ts
@@ -5,6 +5,11 @@ const prisma = new PrismaClient();
 
 export async function GET() {
   try {
+    const walletCount = await prisma.wallet.count();
+    if (walletCount === 0) {
+      return NextResponse.json({ error: "Wallet not found." }, { status: 404 });
+    }
+
     const balance = await prisma.balance.findMany();
     return NextResponse.json<Balance[]>(balance);
   } catch (error) {
