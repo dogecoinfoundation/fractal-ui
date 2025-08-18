3a6885eeff98d2b5c5f6a3dae3563e958e9498b8
Tim Douglas <neurotech@gmail.com>
2025-08-19 09:20:22 +1000

    Wrap Main components in WalletContext

--- components/main.tsx
+++ components/main.tsx
@@ -6,14 +6,19 @@ import { PasswordDialog } from "@/components/password/password-dialog";
 import { Separator } from "@/components/separator";
 import { SideBar } from "@/components/sidebar";
 import { AuthContext } from "@/context/auth-context";
+import { WalletContext } from "@/context/wallet-context";
+import type { Wallet } from "@/generated/prisma";
+import { useAPI } from "@/hooks/useAPI";
 
 export const Main = ({ children }: { children: ReactNode }) => {
   const [password, setPassword] = useState<string | undefined>(undefined);
+  const { data, mutate: refreshWalletData } = useAPI<Wallet>("/api/wallet");
+  const walletAddress = data?.address;
 
   return (
     <AuthContext value={{ password, setPassword }}>
       {password ? (
-        <>
+        <WalletContext value={{ walletAddress, refreshWalletData }}>
           <SideBar />
           <main className="flex flex-col flex-1">
             <div className="flex flex-col h-screen gap-3 p-4">
@@ -22,7 +27,7 @@ export const Main = ({ children }: { children: ReactNode }) => {
               {children}
             </div>
           </main>
-        </>
+        </WalletContext>
       ) : (
         <PasswordDialog />
       )}
