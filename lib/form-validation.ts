4ce977e081e4aa8b881f7e92eb5ea510f23658d2
Tim Douglas <neurotech@gmail.com>
2025-08-19 09:14:05 +1000

    Removed form-validation helpers and tests
    Unneeded

--- lib/form-validation.ts
+++
@@ -1,6 +0,0 @@
-import z from "zod";
-
-export const NumberSchema = (minimumValue: number) =>
-  z.string().refine((value) => parseFloat(value) >= minimumValue, {
-    error: `Must be a number greater than ${minimumValue - 1}.`,
-  });
