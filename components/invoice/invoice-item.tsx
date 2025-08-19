03c2fcf6e6c96afd69f7679d79a66f8b79e5eff4
Tim Douglas <neurotech@gmail.com>
2025-08-19 10:42:23 +1000

    Refactored, re-styled InvoiceTile

---
+++ components/invoice/invoice-item.tsx
@@ -0,0 +1,52 @@
+import { cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const invoiceItemContainerVariants = cva(
+  "flex flex-row items-center flex-1 truncate border-1 rounded-sm",
+  {
+    variants: {
+      variant: {
+        blue: "border-blue-700/20",
+        green: "border-green-700/20",
+        amber: "border-amber-700/20",
+      },
+    },
+  },
+);
+
+const invoiceItemLabelVariants = cva("min-w-28 p-2 font-semibold border-r-1", {
+  variants: {
+    variant: {
+      blue: "border-blue-900/10 bg-blue-100 text-blue-800",
+      green: "border-green-900/10 bg-green-100 text-green-800",
+      amber: "border-amber-900/10 bg-amber-100 text-amber-800",
+    },
+  },
+});
+
+const invoiceItemValueVariants = cva("flex-1 p-2 font-mono tabular-nums", {
+  variants: {
+    variant: {
+      blue: "border-blue-50 bg-blue-50 text-blue-900",
+      green: "border-green-50 bg-green-50 text-green-900",
+      amber: "border-amber-50 bg-amber-50 text-amber-900",
+    },
+  },
+});
+
+export const InvoiceItem = ({
+  label,
+  value,
+  variant,
+}: {
+  label: string;
+  value: string;
+  variant: "blue" | "green" | "amber";
+}) => {
+  return (
+    <div className={cn(invoiceItemContainerVariants({ variant }))}>
+      <div className={cn(invoiceItemLabelVariants({ variant }))}>{label}</div>
+      <div className={cn(invoiceItemValueVariants({ variant }))}>{value}</div>
+    </div>
+  );
+};
--- components/invoice/invoice-tile.tsx
+++ components/invoice/invoice-tile.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns/format";
-import { CalendarDays } from "lucide-react";
+import { CalendarDays, Clock } from "lucide-react";
+import { InvoiceItem } from "@/components/invoice/invoice-item";
 import type { Invoice } from "@/lib/definitions";
 import { cn } from "@/lib/utils";
 
@@ -18,19 +19,6 @@ const InvoiceType = ({ selling }: { selling: boolean }) => {
   );
 };
 
-const InvoiceItem = ({ label, value }: { label: string; value: string }) => {
-  return (
-    <div className="flex flex-row items-center">
-      <div className="min-w-30 p-2 rounded-l-sm border-l-1 border-blue-100 bg-blue-100 text-blue-800 font-semibold">
-        {label}
-      </div>
-      <div className="flex-1 p-2 rounded-r-sm border-r-1 border-blue-50 bg-blue-50 text-blue-900 font-mono tabular-nums">
-        {value}
-      </div>
-    </div>
-  );
-};
-
 export const InvoiceTile = ({
   invoice,
   selling,
@@ -40,25 +28,54 @@ export const InvoiceTile = ({
 }) => {
   return (
     <div className="flex flex-row gap-2 justify-between border-1 border-zinc-300 rounded-sm bg-white">
-      <div className="flex flex-col flex-1 text-sm">
+      <div className="flex flex-col flex-1 text-xs">
         <div className="flex flex-row flex-1 justify-between items-center gap-2 text-zinc-600 border-b-1 border-b-zinc-200 p-1.5">
-          <div className="flex flex-row items-center gap-1 font-semibold">
-            <CalendarDays className="size-3.5 shrink-0 text-zinc-400" />
-            <p>
-              {`${format(new Date(invoice.createdAt), "MMMM dd, yyyy '-' h:mm a")}`}
-            </p>
+          <div className="flex flex-row gap-4 font-semibold">
+            <div className="flex flex-row items-center gap-1">
+              <CalendarDays className="size-3.5 shrink-0 text-zinc-400" />
+              <p>{`${format(new Date(invoice.createdAt), "MMMM dd, yyyy")}`}</p>
+            </div>
+
+            <div className="flex flex-row items-center gap-1">
+              <Clock className="size-3.5 shrink-0 text-zinc-400" />
+              <p>{`${format(new Date(invoice.createdAt), "h:mm a")}`}</p>
+            </div>
           </div>
           <InvoiceType selling={selling} />
         </div>
         <div className="flex flex-col gap-2 p-2">
-          <InvoiceItem
-            label="Quantity"
-            value={invoice.quantity.toLocaleString()}
-          />
-          <InvoiceItem label="Price" value={invoice.price.toLocaleString()} />
-          <InvoiceItem label="Invoice Hash" value={invoice.invoiceHash} />
-          <InvoiceItem label="Mint Hash" value={invoice.mintHash} />
-          <InvoiceItem label="Seller Address" value={invoice.sellerAddress} />
+          <div className="flex flex-row gap-2 justify-between">
+            <InvoiceItem
+              label="Quantity"
+              value={invoice.quantity.toLocaleString()}
+              variant="green"
+            />
+            <InvoiceItem
+              label="Price"
+              value={invoice.price.toLocaleString()}
+              variant="green"
+            />
+          </div>
+
+          <div className="flex flex-col xl:flex-row gap-2 justify-between">
+            <InvoiceItem
+              label="Invoice Hash"
+              value={invoice.invoiceHash}
+              variant="blue"
+            />
+            <InvoiceItem
+              label="Mint Hash"
+              value={invoice.mintHash}
+              variant="blue"
+            />
+          </div>
+          {!selling ? (
+            <InvoiceItem
+              label="Seller Address"
+              value={invoice.sellerAddress}
+              variant="amber"
+            />
+          ) : null}
         </div>
       </div>
     </div>
