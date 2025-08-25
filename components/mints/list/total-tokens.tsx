8f4c912f333b6eef9b3597bd112dde7b43999c92
Tim Douglas <neurotech@gmail.com>
2025-08-25 11:24:07 +1000

    DRY up TotalTokens

--- components/mints/list/total-tokens.tsx
+++ components/mints/list/total-tokens.tsx
@@ -5,12 +5,14 @@ export const TotalTokens = ({
   fractionCount: number;
   balance?: number;
 }) => {
+  const formattedFractionCount = fractionCount.toLocaleString();
+
   return (
     <div className="w-fit flex flex-col gap-0.5 rounded-sm border-1 border-green-600/30 bg-green-50 p-1">
       <h4 className="font-mono text-xs text-green-700 tabular-nums">
         {balance
-          ? `${balance.toLocaleString()} of ${fractionCount.toLocaleString()}`
-          : fractionCount.toLocaleString()}
+          ? `${balance.toLocaleString()} of ${formattedFractionCount}`
+          : formattedFractionCount}
       </h4>
       <h5 className="text-xs font-semibold text-green-700/85">
         {balance ? "Tokens owned" : "Total Tokens"}
